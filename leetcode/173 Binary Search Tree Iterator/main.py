1fa6595b86a0d8c97c11412891cb017d45e8baee
Jeniffer Martins <jeniffermartins.dev@gmail.com>
2024-08-05 21:48:17 -0300

    feat: solution to 173 Binary Searh Tree Iterator

---
+++ leetcode/173 Binary Search Tree Iterator/main.py
@@ -0,0 +1,45 @@
+"""
+    Implement the BSTIterator class that represents an iterator over 
+    the in-order traversal of a binary search tree (BST):
+
+    - BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. 
+        The root of the BST is given as part of the constructor.
+        The pointer should be initialized to a non-existent number smaller 
+        than any element in the BST.
+    - boolean hasNext() Returns true if there exists a number in the traversal
+        to the right of the pointer, otherwise returns false.
+    - int next() Moves the pointer to the right, then returns the number at the pointer.
+
+    Notice that by initializing the pointer to a non-existent smallest number, 
+    the first call to next() will return the smallest element in the BST.
+
+    You may assume that next() calls will always be valid. That is, there will 
+    be at least a next number in the in-order traversal when next() is called.
+
+"""
+
+"""
+    SOLUTIONS OBSERVATIONS
+
+    to understand the data structure of a binary tree, enter the /data-structures/binary-tree.py file.
+"""
+
+class BSTIterator:
+
+    def __init__(self, root):
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    def next(self) -> int:
+        res = self.stack.pop()
+        cur = res.right
+        while cur:
+            self.stack.append(cur)
+            cur = cur.left
+
+        return res.data
+
+    def hasNext(self) -> bool:
+        return self.stack != []

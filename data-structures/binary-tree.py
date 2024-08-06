2702412bda8a94847fda355c6ecfee1dcceac530
Jeniffer Martins <jeniffermartins.dev@gmail.com>
2024-08-05 21:47:25 -0300

    feat: binary tree data structure with BST iterator

--- data-structures/binary-tree.py
+++ data-structures/binary-tree.py
@@ -1,7 +1,3 @@
-# tree = ['g', 'c', 'b', 'a', 'e', 'd', 'f', 'i', 'h', 'j', 'k']
-# tree = [3,9,20,None,None,15,7]
-tree = [1,None,2]
-
 class Node:
     def __init__(self, data):
         self.left  = None
@@ -49,12 +45,62 @@ def maxDepth(root):
 
     return max(l, r)
 
+def isSameTree(p: Node, q: Node) -> bool:
+    if not p and not q:
+        return True
+    if not p or not q or p.data != q.data:
+        return False
 
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
 
-root = Node(tree[0])
+# tree = ['g', 'c', 'b', 'a', 'e', 'd', 'f', 'i', 'h', 'j', 'k']
+# tree = [1,None,2]
+# tree = [3,9,20,None,None,15,7]
 
-for n in range(1,len(tree)):
-    root.insert(tree[n])
+# root = init_tree(tree)
+# maxDepth(root)
 
-maxDepth(root)
+# p = [1,2,3]
+# q = [1,2,3]
 
+# p_root = init_tree(p)
+# q_root = init_tree(q)
+
+# print(isSameTree(p_root, q_root))
+
+#--------------------------------------------------------------
+
+class BSTIterator:
+
+    def __init__(self, root):
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    def next(self) -> int:
+        res = self.stack.pop()
+        cur = res.right
+        while cur:
+            self.stack.append(cur)
+            cur = cur.left
+
+        return res.data
+
+    def hasNext(self) -> bool:
+        return self.stack != []
+
+bst_tree = [7, 3, 15, None, None, 9, 20]
+
+root_bst = init_tree(bst_tree)
+
+bSTIterator = BSTIterator(root_bst)
+print(bSTIterator.next())   
+print(bSTIterator.next())   
+print(bSTIterator.hasNext())
+print(bSTIterator.next())   
+print(bSTIterator.hasNext())
+print(bSTIterator.next())   
+print(bSTIterator.hasNext())
+print(bSTIterator.next())   
+print(bSTIterator.hasNext())
